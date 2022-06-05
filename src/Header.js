eb15f696f5cdfa9c4c2ece2d57c058d93273f46d
walid kibal <walid.yami@gmail.com>
2022-06-05 15:33:43 +0100

    intial commit

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,34 @@
 import React from 'react'
-
+import styled from 'styled-components'
 function Header() {
   return (
     <div>
-        Header
+      <Container>
+        <HeaderLogo>
+<img src={"./Amazon-Logo-1024x426.png"}></img>
+        </HeaderLogo>
+        <HeaderOptionAdress>
+          <OptionLineOne>Hello</OptionLineOne>
+          <OptionLineTow>Select Your Adress</OptionLineTow>
+
+        </HeaderOptionAdress>
+        </Container>
         </div>
   )
 }
 
 export default Header
+const Container = styled.div`
+height :60px;
+background-color : #0f1111 ;
+display : flex;
+align-items : center;
+`
+const HeaderLogo = styled.div `
+img{
+  width:100px;s
+}
+`
+const HeaderOptionAdress = styled.div``
+const OptionLineOne =styled.div``
+const OptionLineTow =styled.div``
