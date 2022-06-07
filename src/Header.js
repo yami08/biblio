b7b6f45eb36a8b85890c7a9793a5a2430184bfe0
walid kibal <walid.yami@gmail.com>
2022-06-07 18:41:29 +0100

    sec commit

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import logo from './Amazon.png'
 import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 function Header() {
   return (
@@ -11,17 +13,36 @@ function Header() {
           <img src={logo}></img>
           
         </HeaderLogo>
-        <HeaderOptionAdress>
-          <OptionLineOne>Hello</OptionLineOne>
-          <OptionLineTow>Select Your Adress</OptionLineTow>
+            <HeaderOptionAdress>
 
-        </HeaderOptionAdress>
+              <LocationOnIcon/>
+              <HeaderOption>
+               <OptionLineOne>Hello</OptionLineOne>
+               <OptionLineTow>Select Your Adress</OptionLineTow>
+               </HeaderOption>
+            </HeaderOptionAdress>
         <HeaderSearch>
           <HeaderSearchInput type='text' />
           <HeaderSearchIconContainer>
             <SearchIcon />
         </HeaderSearchIconContainer>
         </HeaderSearch>
+        <HeaderNavItems>
+        <HeaderOption>
+          <OptionLineOne>Hello Walid</OptionLineOne>
+          <OptionLineTow>Accounts & lists</OptionLineTow>
+
+        </HeaderOption>
+        <HeaderOption>
+          <OptionLineOne>Returns</OptionLineOne>
+          <OptionLineTow>& Orders</OptionLineTow>
+
+        </HeaderOption>
+           <HeaderOptionCart>
+            <ShoppingBasketIcon/>
+            <CartCount>1</CartCount>
+           </HeaderOptionCart>
+        </HeaderNavItems>
         
         </Container>
         </div>
@@ -34,6 +55,7 @@ height :60px;
 background-color : #0f1111 ;
 display : flex;
 align-items : center;
+justify-content : space-between;
 color : white;
 `
 const HeaderLogo = styled.div `
@@ -43,10 +65,54 @@ img{
 }
 `
 const HeaderOptionAdress = styled.div`
+  padding-left : 9px;
+  display : flex;
+  align-items : center;
 
 `
 const OptionLineOne =styled.div``
-const OptionLineTow =styled.div``
-const HeaderSearch= styled.div``
-const HeaderSearchInput =styled.input``
-const HeaderSearchIconContainer =styled.div``
+const OptionLineTow =styled.div`
+  font-weight:700;
+`
+const HeaderSearch= styled.div`
+display : flex;
+flex-grow : 1;
+height : 40px;
+border-radius : 4px;
+overflow:hidden;
+margin-left: 5px;
+background-color : white;
+:focus-within{
+  box-shadow : 0 0 0 3px #f90;
+
+}
+`
+const HeaderSearchInput =styled.input`
+flex-grow : 1;
+border=0;
+:focus {
+  outline: none;
+  }
+`
+const HeaderSearchIconContainer =styled.div`
+background-color : #febd69 ;
+width : 45px ;
+color : black;
+display : flex;
+justify-content :center;
+align-items : center;
+`
+const HeaderNavItems =styled.div`
+display : flex;
+`
+const HeaderOption=styled.div`
+    padding:10px 9px 10px 9px;
+`
+const HeaderOptionCart=styled.div`
+display : flex;
+align-items : center;
+padding-right: 9px;
+`
+const CartCount=styled.div`
+padding-left: 4px;
+`
